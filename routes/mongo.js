45b290ffa760fc7c228bef989ba9cd9743021d34
RaghavRaman <rghv.raman@gmail.com>
2018-04-14 21:12:53 -0400

    MONGO CONNECTION ADDED

--- routes/mongo.js
+++ routes/mongo.js
@@ -6,39 +6,36 @@ var mongoose = require('mongoose'),
 mongoose.Promise = require('bluebird');
 
 //Database connection - Here TEST is the database name-  change it as per your need
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/adbms');
 
 //GETTING THE CONNECTION
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+  console.log("Connected to MONGO");
 });
 
 
-//DEFINE THE SCHEMA OF THE EACH COLLECTION
-var ratingSchema = mongoose.Schema({
-    userID: Number,
-    movieID: Number,
-    rating: Number,
-    timestamp: Number
+var factSchema = mongoose.Schema({
+
+	game_id : Number,
+	time_id : Number,
+	average_score : Number,
+	count_editor_choice : Number
 });
 
 
-//CREATE A MODEL FOR COLLECTION - used as an object of each collections
-var Rating = mongoose.model('ratings', ratingSchema);
+var Fact = mongoose.model('fact',factSchema,'fact');
 
-//THis url will display the follwowing result of the mongo query
 router.get('/mongoquery1', function(req, res) {
-	console.log("QUERY 1  - Average Rating of each movie")
-	Rating.aggregate([{ $group: { _id: '$movieID', avg: { $avg: "$rating" } } }]).limit(20)
-    .then(function(ratings) {
-       	res.send(ratings);
+
+    Fact.find().limit(20).then(function(fact){
+    	console.log(fact);
+    	res.send(fact);
     }).catch(function(error) {
         console.error(error);
     });
 
 });
-
 
 module.exports = router;
